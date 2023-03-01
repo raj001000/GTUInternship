6f29538eac48952af2895955df1fa661006b9ee0
raj001000 <126560645+raj001000@users.noreply.github.com>
2023-03-01 11:06:22 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[7]:
+
+
+arr = np.array([[1,2,3],[3,4,5],[5,6,2],[7,8,9]])
+arr.strides
+
+
+# In[9]:
+
+
+cric_data = np.loadtxt("cric.tsv",skiprows = 1)
+cric_data.show(https://github.com/goradbj/MachineLearning/blob/main/cric.tsv)
+
+
+# In[5]:
+
+
+file = np.loadtxt("https://github.com/goradbj/MachineLearning/blob/main/cric.tsv",skiprows=1)
+
+
+# In[4]:
+
+
+file
+
+
+# In[6]:
+
+
+import numpy as np
+file = np.loadtxt("https://github.com/goradbj/MachineLearning/blob/main/cric.tsv",skiprows=1)
+
+
+# In[7]:
+
+
+file
+
+
+# In[10]:
+
+
+import numpy as np
+cric_data = np.loadtxt("",skiprows = 1)
+cric_data.show(https://github.com/goradbj/MachineLearning/blob/main/cric.tsv)
+
+
+# In[11]:
+
+
+import numpy as np
+file = np.loadtxt("https://github.com/goradbj/MachineLearning/blob/main/cric.tsv",skiprows=1)
+
+
+# In[12]:
+
+
+India =cric_data[,3]
+
+
+# In[ ]:
+
+
+
+
